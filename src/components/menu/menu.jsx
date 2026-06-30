29dfcca9005cfd26be0d022e34ec19cc42619f06
Chichi3779 <johnrosey4@gmail.com>
2026-06-30 11:42:33 +0100

    adjusted the prices

--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -13,8 +13,8 @@ const Menu = () => {
         { id: 102, name: "Moi-moi with Pap\\Custard\\Quaker Oats", price: "2200", image: "/b2.png" },
         { id: 103, name: "Hot Okpa with Pap\\Custard\\Quaker Oats", price: "2200", image: "/b3.png" },
         { id: 104, name: "Yam and Egg Sauce", price: "2900", image: "/b4.png" },
-        { id: 105, name: "White Rice & peppered stew\\Banga stew\\green veggies", price: "1500", image: "/b5.png" },
-        { id: 106, name: "Bread Sandwich toast with Lipton\\Coffee\\green tea\\milk & milo", price: "2500", image: "/b6.png" },
+        { id: 105, name: "White Rice & peppered stew\\Banga stew\\green veggies", price: "1700", image: "/b5.png" },
+        { id: 106, name: "Bread Sandwich toast with Lipton\\Coffee\\green tea\\milk & milo", price: "2700", image: "/b6.png" },
         { id: 107, name: "Beans and Plantain porridge", price: "2200", image: "/b7.png" },
         { id: 108, name: "Fried Plantain and egg sauce", price: "2900", image: "/b8.png" },
         { id: 109, name: "Stir Fried Indomie noodles garnished with fried eggs \\ veggies", price: "2200", image: "/b9.png" },
@@ -23,14 +23,14 @@ const Menu = () => {
     {
       category: "Lunch\\Dinner",
       items: [
-        { id: 201, name: "Native rice", price: "900", image: "/l1.png" },
-        { id: 202, name: "Jollof Rice", price: "1000", image: "/l2.png" },
+        { id: 201, name: "Native rice", price: "1200", image: "/l1.png" },
+        { id: 202, name: "Jollof Rice", price: "1200", image: "/l2.png" },
         { id: 203, name: "Fried Rice", price: "1050", image: "/l3.png" },
         { id: 204, name: "Yam Porridge", price: "2200", image: "/l4.png" },
         { id: 205, name: "Fisherman Porridge yam", price: "2200", image: "/l5.png" },
         { id: 206, name: "Special Coconut Rice", price: "2200", image: "/l6.png" },
-        { id: 207, name: "Spaghetti with veggies", price: "2000", image: "/l7.png" },
-        { id: 208, name: "Beans Porridge", price: "1500", image: "/l8.png" },
+        { id: 207, name: "Spaghetti with veggies", price: "2200", image: "/l7.png" },
+        { id: 208, name: "Beans Porridge", price: "2200", image: "/l8.png" },
         { id: 209, name: "Plaintain Porridge", price: "2200", image: "/l9.png" },
         { id: 210, name: " Boiled Yam and Ugba stew", price: "3900", image: "/n16.png" },
       ]
@@ -39,9 +39,9 @@ const Menu = () => {
     category: "Proteins",
     items: [
       { id: 301, name: "Beef", price: "500", image: "/p1.png" },
-      { id: 302, name: "Chicken", price: "1800", image: "/p2.png" },
+      { id: 302, name: "Chicken", price: "2000", image: "/p2.png" },
       { id: 303, name: "Fried Fish", price: "2000", image: "/p3.png" },
-      { id: 304, name: "Goat Meat", price: "500", image: "/p4.png" },
+      { id: 304, name: "Goat Meat", price: "700", image: "/p4.png" },
       { id: 305, name: "Kpomo", price: "500", image: "/p5.png" },
       { id: 306, name: "Roundabout\\Towel\\Liver", price: "500", image: "/p6.png" },
     ]
@@ -50,14 +50,15 @@ const Menu = () => {
 {
   category: "Soups",
   items: [
-    { id: 701, name: "Egusi", price: "1000", image: "/soup1.png" },
-    { id: 702, name: "Oha", price: "1000", image: "/soup4.png" },
-    { id: 703, name: "Bitterleaf", price: "1000", image: "/soup7.jpg" },
-    { id: 704, name: "Nsala and soup", price: "1500", image: "/soup9.jpg" },
-    { id: 705, name: "Afang soup", price: "1500", image: "/soup10.jpg" },
-    { id: 706, name: "Edikaikong", price: "1500", image: "/soup5.jpg" },
-    { id: 707, name: "Vegetable soup", price: "1500", image: "/soup2.png" },
-    { id: 708, name: "Vegetable okro", price: "1500", image: "/soup8.jpg" },
+    { id: 701, name: "Egusi", price: "1700", image: "/soup1.png" },
+    { id: 702, name: "Oha", price: "1700", image: "/soup4.png" },
+    { id: 703, name: "Bitterleaf", price: "1700", image: "/soup7.jpg" },
+    { id: 704, name: "Nsala and soup", price: "2200", image: "/soup9.jpg" },
+    { id: 705, name: "Afang soup", price: "1700", image: "/soup10.jpg" },
+    { id: 706, name: "Edikaikong", price: "1700", image: "/soup5.jpg" },
+    { id: 707, name: "Vegetable soup", price: "1700", image: "/soup2.png" },
+    { id: 708, name: "Vegetable okro", price: "1700", image: "/soup8.jpg" },
+    {id: 709, name:"Amala and Ewedu", price: "1700", image: "/soup12.jpg" }
   ]
 },
   {
@@ -75,7 +76,7 @@ const Menu = () => {
     category: "Extras",
     items: [
       { id: 501, name: "Moi-Moi", price: "700", image: "/e1.png" },
-      { id: 502, name: "Extra Rice", price: "500", image: "/e2.png" },
+      { id: 502, name: "Extra Rice", price: "600", image: "/e2.png" },
       { id: 503, name: "Extra Soup", price: "500", image: "/e3.png" },
       { id: 504, name: "Salad", price: "500", image: "/e4.png" },
       { id: 505, name: "Plantain", price: "500", image: "/e5.png" },
@@ -84,20 +85,20 @@ const Menu = () => {
   {
     category: "Native Pot",
     items: [
-      { id: 601, name: "Peppered Goat Meat", price: "3500", image: "/n1.png" },
-      { id: 602, name: "Chicken Pepper Soup", price: "3500", image: "/n2.png" },
-      { id: 603, name: "Chicken Vegetables", price: "4000", image: "/n3.png" },
-      { id: 604, name: "Fresh Fish pepper soup", price: "4000", image: "/n4.png" },
-      { id: 605, name: "Goat Meat Pepper Soup", price: "3500", image: "/n5.png" },
+      { id: 601, name: "Peppered Goat Meat", price: "3700", image: "/n1.png" },
+      { id: 602, name: "Chicken Pepper Soup", price: "4200", image: "/n2.png" },
+      { id: 603, name: "Chicken Vegetables", price: "4200", image: "/n3.png" },
+      { id: 604, name: "Fresh Fish pepper soup", price: "4200", image: "/n4.png" },
+      { id: 605, name: "Goat Meat Pepper Soup", price: "3700", image: "/n5.png" },
       { id: 606, name: "Peppered Chicken", price: "3500", image: "/n6.png" },
-      { id: 607, name: "Nkwobi", price: "3500", image: "/n7.png" },
-      { id: 608, name: "Peppered Goat leg", price: "3500", image: "/n8.png" },
-      { id: 609, name: "Peppered Snail", price: "5000", image: "/n9.png" },
+      { id: 607, name: "Nkwobi", price: "3700", image: "/n7.png" },
+      { id: 608, name: "Peppered Goat leg", price: "3700", image: "/n8.png" },
+      { id: 609, name: "Peppered Snail", price: "5300", image: "/n9.png" },
       { id: 610, name: "Barbecue Fish", price: "20,000", image: "/n15.jpeg" },
-      { id: 611, name: "Peppered Kpomo", price: "3000", image: "/n11.png" },
-      { id: 612, name: "Isiewu", price: "10,000", image: "/n12.jpg" },
-      { id: 613, name: "Vegetable Dry Fish", price: "4000", image: "/n13.jpg" },
-      { id: 614, name: "Vegetable Chicken", price: "4000", image: "/n14.jpg" },
+      { id: 611, name: "Peppered Kpomo", price: "3200", image: "/n11.png" },
+      { id: 612, name: "Isiewu", price: "10,200", image: "/n12.jpg" },
+      { id: 613, name: "Vegetable Dry Fish", price: "4200", image: "/n13.jpg" },
+      { id: 614, name: "Vegetable Chicken", price: "4200", image: "/n14.jpg" },
     ]
   },
   // Add this section to your existing menuData array
